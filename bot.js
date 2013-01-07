a4eac2fce3bb44a8dc76a6a612417cc962cab41d
Fahad Ibnay Heylaal <fahad19@gmail.com>
2013-01-07 02:27:55 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,37 @@
+var config = {
+  server: 'localhost',
+  nick: 'bot',
+  options: {
+    channels: ['#channel'],
+    debug: false
+  }
+};
+
+var irc = require('irc');
+var client = new irc.Client(config.server, config.nick, config.options);
+
+var fs = require('fs');
+
+var processor = function(client, from, to, text, message) {
+  if (text && text.length > 2 && text[0] == '!') {
+    var sendTo = from; // send privately
+    if (to.indexOf('#') > -1) {
+      sendTo = to; // send publicly
+    }
+
+    var action = String(text.split(' ')[0]).replace('!', '');
+    if (fs.existsSync('./actions/' + action + '.js')) { // check if we have an action file
+      var actionFunc = require('./actions/' + action + '.js');
+      var output = actionFunc(client, from, to, text, message);
+      if (output) {
+        client.say(sendTo, output);
+      }
+    } else {
+      client.say(sendTo, 'unknown action');
+    }
+  }
+};
+
+client.addListener('message', function(from, to, text, message) {
+    processor(client, from, to, text, message);
+});
